b4f571b13994df13eb95f1f5ffe4a00e0cc67996
solmaznazarova <solmaznezerli@gmail.com>
2021-06-21 15:44:17 +0400

    main templates

--- app.py
+++ app.py
@@ -1,36 +1,26 @@
-<<<<<<< HEAD
 from flask import Flask, render_template, redirect,url_for,request
-=======
 from flask import Flask, render_template, redirect,url_for, request
->>>>>>> 4b30fc59c521eb8271986fcd3537d6cfbcfeacc7
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from werkzeug.utils import secure_filename
 import os
-<<<<<<< HEAD
 UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static/uploads/')
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
-=======
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
 UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static/uploads/')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
->>>>>>> 4b30fc59c521eb8271986fcd3537d6cfbcfeacc7
 
 # USER INTERFACE ROUTES
 @app.route("/blog-list")
 def blog_list():
     blogs = Blog.query.all()
-<<<<<<< HEAD
     return render_template("blog-list.html",blogs=blogs)
-=======
     return render_template("blog-list.html", blogs=blogs)
->>>>>>> 4b30fc59c521eb8271986fcd3537d6cfbcfeacc7
 
 @app.route("/about-us")
 def about_us():
@@ -72,7 +62,9 @@ def register():
 def shopping_cart():
     return render_template("shopping-cart.html")
 
-<<<<<<< HEAD
+
+
+
 class Blog(db.Model):
 
     __tablename__= "blog"
@@ -83,7 +75,6 @@ class Blog(db.Model):
     published_at = db.Column(db.DateTime,default=datetime.utcnow)
     image = db.Column(db.String(20),default='static/images/avatar-1.jpg')
     
-=======
 
 class Blog(db.Model):
 
@@ -95,27 +86,13 @@ class Blog(db.Model):
     published_at = db.Column(db.DateTime, default = datetime.utcnow)
     image = db.Column(db.String(20), default='static/uploads/download.jpeg')
 
->>>>>>> 4b30fc59c521eb8271986fcd3537d6cfbcfeacc7
+
     def __repr__(self):
         return self.title
 
 # ADMIN PANEL ROUTES
-<<<<<<< HEAD
 
-@app.route("/admin/blog-add",methods=['GET','POST'])
-def admin_blog_add():
-    if request.method =='POST':
-        file = request.files['file']
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        blog=Blog(
-            title = request.form['title'],
-            short_description = request.form['short-desc'],
-            description = request.form['desc'],
-            image = filename
-
-=======
-@app.route("/admin-blog-add", methods=['GET', 'POST'])
+@app.route("/admin-blog-add", methods=['GET', 'POST']) 
 def admin_blog_add():
     if request.method == 'POST':
         file = request.files['file']
@@ -126,17 +103,11 @@ def admin_blog_add():
             short_description = request.form['short-desc'], 
             description = request.form['desc'],
             image = filename
->>>>>>> 4b30fc59c521eb8271986fcd3537d6cfbcfeacc7
         )
         db.session.add(blog)
         db.session.commit()
         return redirect(url_for('blog_list'))
-<<<<<<< HEAD
     return render_template ("admin/blog-add.html")
 
-=======
-    return render_template('admin/blog-add.html')
->>>>>>> 4b30fc59c521eb8271986fcd3537d6cfbcfeacc7
-    
 if __name__ == "__main__" :
     app.run(debug=True)
