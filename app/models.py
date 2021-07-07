fc110514e2110c68d806f8fd3c5b773fb10f9443
solmaznazarova <solmaznezerli@gmail.com>
2021-07-07 17:32:30 +0400

    Pavshop fayllari

--- app/models.py
+++ app/models.py
@@ -11,6 +11,22 @@ class Blog(db.Model):
     published_at = db.Column(db.DateTime, default = datetime.utcnow)
     image = db.Column(db.String(20), default='static/uploads/download.jpeg')
 
+class Product(db.Model):
+
+    __tablename__='product'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.String(100),nullable = False)
+    designer = db.Column(db.String(100),nullable= True)
+    brand = db.Column(db.String(120),nullable= False)
+    short_description = db.Column(db.String(100),nullable = False)
+    description = db.Column(db.Text, nullable=False)
+    image = db.Column(db.String(20), nullable = False)
+
+
+
+
+
 
     def __repr__(self):
         return self.title
--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,33 @@
 from flask import render_template,url_for,redirect,request
 from app import app
 from app import db
-from app.models import Blog
+from app.models import Blog,Product
 from werkzeug.utils import secure_filename
 import os
 UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static/uploads/')
 
 
 # USER INTERFACE ROUTES
+
 @app.route("/blog-list")
 def blog_list():
     blogs = Blog.query.all()
     return render_template("blog-list.html",blogs=blogs)
+
+@app.route("/product-list")
+def productlist():
+    products = Product.query.all()
+    return render_template("product-list.html",products=products)
 
 
 @app.route("/about-us")
 def about_us():
     return render_template("about-us.html")
 
-@app.route("/blog-detail")
-def blog_detail():
-    return render_template("blog-detail.html")
+@app.route("/blog-detail/<int:id>",methods =['GET','POST'])
+def blog_detail(id):
+    blog = Blog.query.get_or_404(id)
+    return render_template("blog-detail.html",blog = blog)
 
 @app.route("/checkout")
 def checkout():
@@ -38,13 +45,11 @@ def index():
 def login():
     return render_template("login.html")
 
-@app.route("/product-detail")
-def product_detail():
-    return render_template("product-detail.html")
 
-@app.route("/product-list")
-def product_list():
-    return render_template("product-list.html")
+@app.route("/product-detail/<int:id>")
+def product_detail(id):
+    product = Product.query.get_or_404(id)
+    return render_template("product-detail.html",product = product)
 
 @app.route("/register")
 def register():
@@ -73,3 +78,84 @@ def admin_blog_add():
         db.session.commit()
         return redirect(url_for('blog_list'))
     return render_template ("admin/blog-add.html")
+
+@app.route("/admin-blog-list",methods=['GET','POST'])
+def admin_blog_list():
+    blogs = Blog.query.all()
+    return render_template("admin/blog-list.html",blogs = blogs)
+
+@app.route("/admin-blog-edit/<int:id>",methods =['GET','POST'])
+def admin_blog_edit(id):
+    blog = Blog.query.get(id)
+    if request.method == ['POST']:
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        blog.title = request.form['title']
+        blog.short_description = request.form['short-desc']
+        blog.description = request.form['desc']
+        blog.image = filename
+        db.session.commit()
+        return redirect(url_for('admin_blog_list'))
+    return render_template('admin/blog-edit.html',blog=blog)
+
+@app.route("/admin-blog-delete/<int:id>",methods =['GET','POST'])
+def admin_blog_delete(id):
+    blog = Blog.query.get(id)
+    db.session.delete(blog)
+    db.session.commit()
+    return redirect(url_for('admin_blog_list'))
+
+ 
+@app.route("/admin-add-product", methods=['GET', 'POST']) 
+def admin_add_product():
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        product = Product(
+            title = request.form['title'],
+            price = request.form['price'],
+            designer = request.form['designer'],
+            brand = request.form['brand'],
+            short_description = request.form['short-desc'], 
+            description = request.form['desc'],
+            image = filename
+        )
+        db.session.add(product)
+        db.session.commit()
+        return redirect(url_for('productlist'))
+    return render_template ("admin/add_product.html")
+
+
+@app.route("/admin-product-list",methods=['GET','POST'])
+def admin_product_list():
+    products = Product.query.all()
+    return render_template("admin/product-list.html",products = products)
+
+
+
+@app.route("/admin-product-edit/<int:id>",methods =['GET','POST'])
+def admin_product_edit(id):
+    product = Product.query.get(id)
+    if request.method == ['POST']:
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        product.title = ['title']
+        product.price = ['price']
+        product.designer = ['designer']
+        product.brand = ['brand']
+        product.short_description = ['short-desc']
+        product.description = ['desc']
+        product.image = filename
+        db.session.commit()
+        return redirect(url_for('admin_product_list'))
+    return render_template('admin/product-edit.html',product=product)
+
+@app.route("/admin-product-delete/<int:id>",methods =['GET','POST'])
+def admin_product_delete(id):
+    product = Product.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+    return redirect(url_for('admin_product_list'))
